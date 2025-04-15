72261fafb9d1fc47ea6d4a4ab9642bb8a8366f68
Carlos Urias <hicarlosurias@gmail.com>
2025-04-15 13:09:32 -0700

    feat: tailwind css agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-// src/App.jsx
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import "./styles/main.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* Más rutas si querés */}
     </Routes>
   );
 }

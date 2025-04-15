1f8ee03173373423d9ad26a9161734cd506a62ad
Carlos Urias <hicarlosurias@gmail.com>
2025-04-15 13:52:04 -0700

    ruta para el login

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,9 +1,14 @@
 import Home from "../pages/Home";
+import Login from "../modules/Login/pages/Login";
 
 const routes = [
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
   },
 ];
 
